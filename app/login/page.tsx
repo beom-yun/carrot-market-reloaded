d57849757b0ff46253641e3de7938a1f28d512dc
beom-yun <scds0326@naver.com>
2024-04-09 02:29:23 +0900

    Server Actions

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,38 +1,38 @@
-"use client";
-
 import FormInput from "../components/form-input";
 import FormButton from "../components/form.btn";
 import SocialLogin from "../components/social-login";
 
 export default function Login() {
-  const onClick = async () => {
-    const response = await fetch("/api/users", {
-      method: "POST",
-      body: JSON.stringify({
-        username: "yun",
-        password: "1234",
-      }),
-    });
-    console.log(await response.json());
+  const handleForm = async (formData: FormData) => {
+    "use server";
+    console.log("I run in the server");
+    console.log(formData.get("email"), formData.get("password"));
   };
+
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-semibold">
         <h1 className="text-2xl">안녕하세요!</h1>
         <h2 className="text-xl">다시 오신걸 환영합니다!</h2>
       </div>
-      <form className="flex flex-col gap-3">
-        <FormInput type="email" placeholder="이메일" required errors={[]} />
+      <form action={handleForm} className="flex flex-col gap-3">
         <FormInput
+          name="email"
+          type="email"
+          placeholder="이메일"
+          required
+          errors={[]}
+        />
+        <FormInput
+          name="password"
           type="password"
           placeholder="비밀번호"
           required
           errors={[]}
         />
-      </form>
-      <span onClick={onClick}>
         <FormButton loading={false} text="로그인하기" />
-      </span>
+      </form>
+
       <SocialLogin />
     </div>
   );
