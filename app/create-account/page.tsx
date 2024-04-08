e39d9f99716dda9d550aa8c7f0eb2bb3eb7d5fb3
beom-yun <scds0326@naver.com>
2024-04-08 14:45:33 +0900

    Form Components

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,24 +1,31 @@
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import Link from "next/link";
+import FormInput from "../components/form-input";
+import FormButton from "../components/form.btn";
 
 export default function CreateAccount() {
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-semibold">
         <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">아래 양식을 채워주세요!</h2>
+        <h2 className="text-xl">아래 양식을 채워서 가입하세요!</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <div className="flex flex-col gap-2">
-          <input
-            type="text"
-            placeholder="아이디"
-            required
-            className="h-10 w-full rounded-md border-none bg-transparent ring-1 ring-neutral-200 placeholder:text-neutral-400 focus:outline-none focus:ring-2 focus:ring-orange-500"
-          />
-          <span className="font-medium text-red-500">입력 에러</span>
-        </div>
-        <button className="primary-btn h-10">계정 만들기</button>
+        <FormInput type="text" placeholder="아이디" required errors={[]} />
+        <FormInput type="email" placeholder="이메일" required errors={[]} />
+        <FormInput
+          type="password"
+          placeholder="비밀번호"
+          required
+          errors={[]}
+        />
+        <FormInput
+          type="password"
+          placeholder="비밀번호 확인"
+          required
+          errors={[]}
+        />
+        <FormButton loading={false} text="계정 만들기" />
       </form>
       <div className="h-px w-full bg-neutral-500" />
       <div>
