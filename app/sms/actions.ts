8a81abdcf7f308a8fb832496368d35e36aec38cd
beom-yun <scds0326@naver.com>
2024-04-12 16:01:54 +0900

    SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,13 +1,49 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
 
-const phoneSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneSchema = z
+  .string()
+  .trim()
+  .refine(
+    phone => validator.isMobilePhone(phone, "ko-KR"),
+    "Wrong phone format",
+  );
 
 const tokenSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsVerification(prevState: any, formData: FormData) {
-  console.log(typeof formData.get("token"));
-  console.log(typeof tokenSchema.parse(formData.get("token")));
+interface IActionState {
+  token: boolean;
+}
+
+export async function smsVerification(
+  prevState: IActionState,
+  formData: FormData,
+) {
+  const phone = formData.get("phone");
+  const token = formData.get("token");
+
+  if (!prevState.token) {
+    const result = phoneSchema.safeParse(phone);
+    if (!result.success) {
+      return {
+        token: false,
+        error: result.error.flatten(),
+      };
+    } else {
+      return { token: true };
+    }
+  } else {
+    const result = tokenSchema.safeParse(token);
+    if (!result.success) {
+      return {
+        token: true,
+        error: result.error.flatten(),
+      };
+    } else {
+      redirect("/");
+    }
+  }
 }
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,13 @@ import Button from "../components/button";
 import { smsVerification } from "./actions";
 import { useFormState } from "react-dom";
 
+const initialState = {
+  token: false,
+  error: undefined,
+};
+
 export default function SMSLogin() {
-  const [state, dispatch] = useFormState(smsVerification, null);
+  const [state, dispatch] = useFormState(smsVerification, initialState);
 
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
@@ -15,16 +20,27 @@ export default function SMSLogin() {
         <h2 className="text-xl">핸드폰 인증을 해주세요!</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <Input name="phone" type="number" placeholder="핸드폰 번호" required />
-        <Input
-          name="token"
-          type="text"
-          placeholder="확인코드"
-          required
-          min={100000}
-          max={999999}
-        />
-        <Button text="인증하기" />
+        {state.token ? (
+          <Input
+            key="token"
+            name="token"
+            type="text"
+            placeholder="확인코드"
+            required
+            min={100000}
+            max={999999}
+          />
+        ) : (
+          <Input
+            key="phone"
+            name="phone"
+            type="number"
+            placeholder="핸드폰 번호"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <Button text={state.token ? "인증하기" : "인증번호 보내기"} />
       </form>
     </div>
   );
