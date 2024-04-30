33fb6aa80fe73a73bdcc274d9cb248a8f9272c3d
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-04-30 16:44:01 +0900

    Reset

--- app/(tabs)/life/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Life() {
-  return (
-    <div>
-      <h1 className="text-4xl text-white">Life!</h1>
-    </div>
-  );
-}
--- app/(tabs)/live/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Live() {
-  return (
-    <div>
-      <h1 className="text-4xl text-white">Live!</h1>
-    </div>
-  );
-}
