0653d60c2966332b6a550808c3078e42f1ba4b0f
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-01 23:18:52 +0900

    Log in Screen

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,15 +2,14 @@ import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
 
-export default function CreateAccount() {
+export default function LogIn() {
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
       <div className="flex flex-col gap-2 *:font-medium">
-        <h1 className="text-2xl">안녕하세요!</h1>
-        <h2 className="text-xl">아래 양식에 작성해주세요!</h2>
+        <h1 className="text-2xl">반갑습니다!</h1>
+        <h2 className="text-xl">다시 오신걸 환영합니다!</h2>
       </div>
       <form className="flex flex-col gap-3">
-        <FormInput type="text" placeholder="아이디" required errors={[]} />
         <FormInput type="email" placeholder="이메일" required errors={[]} />
         <FormInput
           type="password"
@@ -18,13 +17,7 @@ export default function CreateAccount() {
           required
           errors={[]}
         />
-        <FormInput
-          type="password"
-          placeholder="비밀번호 확인"
-          required
-          errors={[]}
-        />
-        <FormButton loading={false} text="계정 만들기" />
+        <FormButton loading={false} text="로그인" />
       </form>
       <SocialLogin />
     </div>
---
+++ app/sms/page.tsx
@@ -0,0 +1,23 @@
+import FormButton from "@/components/form-btn";
+import FormInput from "@/components/form-input";
+
+export default function SMSLogIn() {
+  return (
+    <div className="flex flex-col gap-10 px-6 py-8">
+      <div className="flex flex-col gap-2 *:font-medium">
+        <h1 className="text-2xl">SMS 로그인</h1>
+        <h2 className="text-xl">핸드폰을 이용해 로그인해주세요!</h2>
+      </div>
+      <form className="flex flex-col gap-3">
+        <FormInput
+          type="number"
+          placeholder="핸드폰 번호"
+          required
+          errors={[]}
+        />
+        <FormInput type="number" placeholder="인증 코드" required errors={[]} />
+        <FormButton loading={false} text="인증하기" />
+      </form>
+    </div>
+  );
+}
