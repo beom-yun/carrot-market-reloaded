bbcd9b805ec05084556c2c42082a8f6b262d69d9
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-02 01:33:21 +0900

    useFormState

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,13 @@
+"use client";
+
 import FormButton from "@/components/form-btn";
 import FormInput from "@/components/form-input";
 import SocialLogin from "@/components/social-login";
+import { useFormState } from "react-dom";
+import { handleForm } from "./actions";
 
 export default function LogIn() {
-  const handleForm = async (formData: FormData) => {
-    "use server";
-    await new Promise(resolve => setTimeout(resolve, 5000));
-    console.log(formData.get("email"), formData.get("password"));
-  };
+  const [state, dispatch] = useFormState(handleForm, null);
 
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
@@ -15,7 +15,7 @@ export default function LogIn() {
         <h1 className="text-2xl">반갑습니다!</h1>
         <h2 className="text-xl">다시 오신걸 환영합니다!</h2>
       </div>
-      <form action={handleForm} className="flex flex-col gap-3">
+      <form action={dispatch} className="flex flex-col gap-3">
         <FormInput
           type="email"
           name="email"
@@ -28,7 +28,7 @@ export default function LogIn() {
           name="password"
           placeholder="비밀번호"
           required
-          errors={[]}
+          errors={state?.errors ?? []}
         />
         <FormButton text="로그인" />
       </form>
