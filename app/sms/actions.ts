e1dea691477725e2a4806c82a8444576e74322ec
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-02 21:25:24 +0900

    SMS Validation

--- app/sms/actions.ts
+++ app/sms/actions.ts
@@ -1,10 +1,40 @@
 "use server";
 
+import { redirect } from "next/navigation";
 import validator from "validator";
 import { z } from "zod";
 
-const phoneNumberSchema = z.string().trim().refine(validator.isMobilePhone);
+const phoneNumberSchema = z
+  .string()
+  .trim()
+  .refine(
+    phone => validator.isMobilePhone(phone, "ko-KR"),
+    "잘못된 핸드폰 번호입니다.",
+  );
 
 const verificationCodeSchema = z.coerce.number().min(100000).max(999999);
 
-export async function smsLogIn(prevState: any, formData: FormData) {}
+export interface IActionState {
+  code: boolean;
+}
+
+export async function smsLogIn(prevState: IActionState, formData: FormData) {
+  const phoneNumber = formData.get("phoneNumber");
+  const verificationCode = formData.get("verificationCode");
+
+  if (!prevState.code) {
+    const result = phoneNumberSchema.safeParse(phoneNumber);
+    if (!result.success) {
+      return { code: false, error: result.error.flatten() };
+    } else {
+      return { code: true };
+    }
+  } else {
+    const result = verificationCodeSchema.safeParse(verificationCode);
+    if (!result.success) {
+      return { code: true, error: result.error.flatten() };
+    } else {
+      redirect("/");
+    }
+  }
+}
--- app/sms/page.tsx
+++ app/sms/page.tsx
@@ -5,8 +5,10 @@ import FormInput from "@/components/form-input";
 import { useFormState } from "react-dom";
 import { smsLogIn } from "./actions";
 
+const initialState = { code: false, error: undefined };
+
 export default function SMSLogIn() {
-  const [state, dispatch] = useFormState(smsLogIn, null);
+  const [state, dispatch] = useFormState(smsLogIn, initialState);
 
   return (
     <div className="flex flex-col gap-10 px-6 py-8">
@@ -15,23 +17,28 @@ export default function SMSLogIn() {
         <h2 className="text-xl">핸드폰을 이용해 로그인해주세요!</h2>
       </div>
       <form action={dispatch} className="flex flex-col gap-3">
-        <FormInput
-          type="text"
-          name="phoneNumber"
-          placeholder="핸드폰 번호"
-          required
-          errors={state?.fieldErrors.phoneNumber}
-        />
-        <FormInput
-          type="number"
-          name="verificationCode"
-          placeholder="인증 코드"
-          required
-          min={100000}
-          max={999999}
-          errors={state?.fieldErrors.verificationCode}
-        />
-        <FormButton text="인증하기" />
+        {state?.code ? (
+          <FormInput
+            key="code"
+            type="number"
+            name="verificationCode"
+            placeholder="인증 코드"
+            required
+            min={100000}
+            max={999999}
+            errors={state.error?.formErrors}
+          />
+        ) : (
+          <FormInput
+            key="phoneNumber"
+            type="text"
+            name="phoneNumber"
+            placeholder="핸드폰 번호"
+            required
+            errors={state.error?.formErrors}
+          />
+        )}
+        <FormButton text={state.code ? "인증하기" : "인증번호 보내기"} />
       </form>
     </div>
   );
