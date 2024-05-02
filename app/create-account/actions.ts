93ae7ad6602fa96189db7dfce48e64c1b242016d
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-02 16:38:24 +0900

    Transformation

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,6 +2,10 @@
 
 import { z } from "zod";
 
+const passwordRegex = new RegExp(
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*?[#?!@$%^&*-]).+$/,
+);
+
 const checkPasswords = ({
   password,
   confirmPassword,
@@ -18,9 +22,17 @@ const formSchema = z
         required_error: "아이디는 필수입니다.",
       })
       .min(3, "아이디가 너무 짧습니다.(최소 3자)")
-      .max(16, "아이디가 너무 깁니다.(최대 16자)"),
-    email: z.string().email(),
-    password: z.string().min(4),
+      .max(16, "아이디가 너무 깁니다.(최대 16자)")
+      .trim()
+      .toLowerCase(),
+    email: z.string().email().toLowerCase(),
+    password: z
+      .string()
+      .min(4)
+      .regex(
+        passwordRegex,
+        "비밀번호는 대문자, 소문자, 숫자, 특수문자를 포함하여야 합니다.",
+      ),
     confirmPassword: z.string().min(4),
   })
   .refine(checkPasswords, {
@@ -38,5 +50,7 @@ export async function createAccount(prevState: any, formData: FormData) {
   const result = formSchema.safeParse(data);
   if (!result.success) {
     return result.error.flatten();
+  } else {
+    console.log(result.data);
   }
 }
