a52659121c7da23f43b1699281e0551dff78cc4c
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-02 16:29:48 +0900

    Refinement

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -2,12 +2,31 @@
 
 import { z } from "zod";
 
-const formSchema = z.object({
-  username: z.string().min(3).max(16),
-  email: z.string().email(),
-  password: z.string().min(4),
-  confirmPassword: z.string().min(4),
-});
+const checkPasswords = ({
+  password,
+  confirmPassword,
+}: {
+  password: string;
+  confirmPassword: string;
+}) => password === confirmPassword;
+
+const formSchema = z
+  .object({
+    username: z
+      .string({
+        invalid_type_error: "아이디는 문자열이어야 합니다.",
+        required_error: "아이디는 필수입니다.",
+      })
+      .min(3, "아이디가 너무 짧습니다.(최소 3자)")
+      .max(16, "아이디가 너무 깁니다.(최대 16자)"),
+    email: z.string().email(),
+    password: z.string().min(4),
+    confirmPassword: z.string().min(4),
+  })
+  .refine(checkPasswords, {
+    message: "패스워드가 일치하지 않습니다.",
+    path: ["confirmPassword"],
+  });
 
 export async function createAccount(prevState: any, formData: FormData) {
   const data = {
