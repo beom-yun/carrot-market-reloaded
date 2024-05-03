073dd55e2fc50b50ee6327a45759c3f720772b7f
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-03 15:41:17 +0900

    Email Log In

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,10 +1,26 @@
 "use server";
 
+import bcrypt from "bcrypt";
 import { PASSWORD_MIN_LENGTH, PASSWORD_REGEX } from "@/lib/constants";
+import db from "@/lib/db";
 import { z } from "zod";
+import getSession from "@/lib/session";
+import { redirect } from "next/navigation";
+
+const checkEmailExists = async (email: string) => {
+  const user = await db.user.findUnique({
+    where: { email },
+    select: { id: true },
+  });
+  return Boolean(user);
+};
 
 const formSchema = z.object({
-  email: z.string().email().toLowerCase(),
+  email: z
+    .string()
+    .email()
+    .toLowerCase()
+    .refine(checkEmailExists, "이 이메일을 사용하는 계정이 존재하지 않습니다."),
   password: z
     .string()
     .min(PASSWORD_MIN_LENGTH)
@@ -19,10 +35,27 @@ export async function login(prevState: any, formData: FormData) {
     email: formData.get("email"),
     password: formData.get("password"),
   };
-  const result = formSchema.safeParse(data);
+  const result = await formSchema.safeParseAsync(data);
   if (!result.success) {
     return result.error.flatten();
   } else {
-    console.log(result.data);
+    const user = await db.user.findUnique({
+      where: { email: result.data.email },
+      select: { id: true, password: true },
+    });
+    const ok = await bcrypt.compare(result.data.password, user!.password ?? "");
+    if (ok) {
+      const session = await getSession();
+      session.id = user!.id;
+      session.save();
+      redirect("/profile");
+    } else {
+      return {
+        fieldErrors: {
+          email: [],
+          password: ["틀린 비밀번호입니다."],
+        },
+      };
+    }
   }
 }
