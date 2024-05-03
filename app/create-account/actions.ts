e2d56130d31f9737abdd47b9bdf551a22ea9d1e8
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-03 14:55:23 +0900

    Iron Session

--- app/create-account/actions.ts
+++ app/create-account/actions.ts
@@ -9,6 +9,9 @@ import {
 } from "@/lib/constants";
 import db from "@/lib/db";
 import { z } from "zod";
+import { getIronSession } from "iron-session";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 const checkPasswords = ({
   password,
@@ -84,8 +87,13 @@ export async function createAccount(prevState: any, formData: FormData) {
       },
       select: { id: true },
     });
-    console.log(user);
-    // log the user in
-    // redirect "/home"
+    const cookie = await getIronSession(cookies(), {
+      cookieName: "delicious-karrot",
+      password: process.env.COOKIE_PASSWORD!,
+    });
+    //@ts-ignore
+    cookie.id = user.id;
+    await cookie.save();
+    redirect("/profile");
   }
 }
