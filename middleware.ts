583bb644b2d4e84f9e4c2d1099344f3c76c86c77
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-04 15:05:35 +0900

    Matcher

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,9 @@
 import { NextRequest } from "next/server";
-import getSession from "./lib/session";
 
 export async function middleware(request: NextRequest) {
-  const session = await getSession();
-  console.log(session);
-  if (request.nextUrl.pathname === "/profile") {
-    console.log("Middle로부터 허락되지 않습니다.");
-    return Response.redirect(new URL("/", request.url));
-  }
+  console.log(request.nextUrl.pathname);
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
