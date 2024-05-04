8e4762f44ac2c7341f05b319cb2011ad6e54c389
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-04 23:40:15 +0900

    Skeletons

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,4 +1,10 @@
-export default function Products() {
+export async function getProducts() {
+  await new Promise(resolve => setTimeout(resolve, 5000));
+}
+
+export default async function Products() {
+  const products = await getProducts();
+
   return (
     <div>
       <h1 className="text-4xl text-white">Product!</h1>
