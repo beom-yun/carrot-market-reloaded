55becc8b88dc67b8d9787b7a3789a48f045e7175
beom-yun <58946778+beom-yun@users.noreply.github.com>
2024-05-11 20:49:07 +0900

    generateStaticParams

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -104,3 +104,10 @@ export default async function ProductDetail({
     </div>
   );
 }
+
+export async function generateStaticParams() {
+  const products = await db.product.findMany({
+    select: { id: true },
+  });
+  return products.map(product => ({ id: product.id + "" }));
+}
